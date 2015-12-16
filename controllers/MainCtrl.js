2a5e23995bba343c79d5a6371f06bcb40d3a800b
Jeremy <jeremygrondahl@gmail.com>
2015-12-16 00:19:50 -0600

    Created main controller

---
+++ controllers/MainCtrl.js
@@ -0,0 +1,19 @@
+define([
+  'angular',
+  'angularRoute'
+], function(angular) {
+  angular.module("Main.portfolio", ['ngRoute'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'partials/main.html',
+      controller: 'MainCtrl'
+    });
+  }]).controller("MainCtrl", 
+  ["$scope",
+  function($scope) {
+    $(document).ready(function (){
+      $('body').css('opacity', '0').fadeTo(1500, 1,'swing'); 
+    });
+  
+  }]);
+});
