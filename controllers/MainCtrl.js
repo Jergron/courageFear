c91401b76f63e404b8bd926f18543d449b462f61
Jeremy <jeremygrondahl@gmail.com>
2016-11-06 08:30:53 -0600

    added random fadein function for the title of page

--- controllers/MainCtrl.js
+++ controllers/MainCtrl.js
@@ -15,6 +15,21 @@ define([
     // $(document).ready(function (){
     //   $('body').css('opacity', '0').fadeTo(1500, 1,'swing'); 
     // });
-    
+    var count = 0; 
+    var maxDelay = 1300;
+    var minSpeed = 1300;
+    var maxSpeed = 2000;
+    var fadeTo = 1;
+    $('div span').each(function(){
+        var delay = Math.ceil(Math.random() * maxDelay);
+        var speed = maxSpeed + Math.ceil(Math.random() * (minSpeed - maxSpeed));
+        count++;
+        $(this).delay(delay).fadeTo(speed, fadeTo, function(){
+            count--;
+            if (count == 0){
+                //onFinish();
+            }
+        });
+    });
   }]);
 });
